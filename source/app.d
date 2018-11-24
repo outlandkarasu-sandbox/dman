cd318b8c2d703ab6b6de7f57d12496559cca6b2c
outlandkarasu <outland.karasu@gmail.com>
2018-11-24 16:09:52 +0900

    issue #5 ウィンドウ作成を実装した。

--- source/app.d
+++ source/app.d
@@ -8,15 +8,40 @@ import dman.sdl :
 import bindbc.sdl :
     SDL_Init,
     SDL_INIT_VIDEO,
-    SDL_Quit
-;
+    SDL_Quit,
+    SDL_CreateWindow,
+    SDL_DestroyWindow,
+    SDL_WINDOW_SHOWN ;
+
+/// ウィンドウタイトル
+enum TITLE = "D-man Viewer";
+
+/// ウィンドウ初期表示位置
+enum {
+    WINDOW_POS_X = 0,
+    WINDOW_POS_Y = 0,
+    WINDOW_WIDTH = 480,
+    WINDOW_HEIGHT = 320,
+}
 
 /// メイン処理
 void main() {
+    // SDLのロード
     immutable loadedVersion = loadSdl();
     writefln("SDL loaded: %s", loadedVersion);
 
+    // 使用するサブシステムの初期化
     enforceSdl(SDL_Init(SDL_INIT_VIDEO));
     scope(exit) SDL_Quit();
+
+    // メインウィンドウ生成
+    auto window = SDL_CreateWindow(
+        TITLE,
+        WINDOW_POS_X,
+        WINDOW_POS_Y,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN); // 作成時に表示する。
+    scope(exit) SDL_DestroyWindow(window);
 }
 
