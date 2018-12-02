b38e467903c613859583184fcd3f7e2787400e5e
outlandkarasu <outland.karasu@gmail.com>
2018-12-02 17:39:44 +0900

    issue #23 translatedを追加した。

--- source/dman/math/mvp.d
+++ source/dman/math/mvp.d
@@ -4,13 +4,14 @@
 module dman.math.mvp;
 
 import mir.ndslice :
+    blocks,
     diagonal,
     flattened,
+    isMatrix,
     ndarray,
-    Slice,
     slice,
-    SliceKind,
-    transposed
+    transposed,
+    zip
 ;
 
 /// 4x4行列を生成する。
@@ -18,11 +19,24 @@ auto mat4(T = float)() {
     return slice!T(4, 4);
 }
 
-/**
- *  OpenGL行列形式(Column-Major)に変換する。
- */
-auto glMat4(Iterator, size_t N, SliceKind kind)(Slice!(Iterator, N, kind) slice)
-if(N == 2)
+/// 単位行列を生成する。
+auto identitied(S)(S slice) if(isMatrix!S)
+in {
+    assert(slice.shape[0] == slice.shape[1]);
+} body {
+    alias T = S.DeepElement;
+    slice[] = cast(T) 0.0;
+    slice.diagonal[] = cast(T) 1.0;
+    return slice;
+}
+
+/// ditto
+auto identity4(T = float)() {
+    return mat4!T.identitied;
+}
+
+/// OpenGL行列形式(Column-Major)に変換する。
+auto glMat4(S)(S slice) if(isMatrix!S)
 in {
     assert(slice.shape == [4, 4]);
 } body {
@@ -31,15 +45,33 @@ in {
 
 ///
 unittest {
-    auto id = mat4();
-    id[] = 0.0f;
-    id.diagonal[] = 1.0f;
-    id[0, 2] = 2.0f;
-    assert(id.glMat4 == [
+    auto m = identity4();
+    m[0, 2] = 2.0f;
+    assert(m.glMat4 == [
         1.0f, 0.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f, 0.0f,
         2.0f, 0.0f, 1.0f, 0.0f,
         0.0f, 0.0f, 0.0f, 1.0f,
+    ]);
+}
+
+/// 平行移動を行う行列の生成。
+auto translated(S, T)(S slice, T x, T y, T z) if(isMatrix!S) {
+    auto m = slice.identitied;
+    m[0, 3] = x;
+    m[1, 3] = y;
+    m[2, 3] = z;
+    return m;
+}
+
+///
+unittest {
+    auto m = identity4.translated(1.0f, 2.0f, 3.0f);
+    assert(m.glMat4 == [
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        1.0f, 2.0f, 3.0f, 1.0f,
     ]);
 }
 
